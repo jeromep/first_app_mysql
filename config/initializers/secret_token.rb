3d33327fdde73f377a4d618a3f3fbcec29bd941d
Jerome Penarubia <jeromepenarubia@Jeromes-MacBook-Pro.local>
2012-03-07 23:37:49 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstAppMysql::Application.config.secret_token = '6635442c39d80198f6116b947f864fdfc3187d1975216bf305296b79d988cfffeb4505a39260cbff77d54877f20af1cd1ec2c2e6a22ffa776ff3ec9a195495d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FirstAppMysql::Application.config.session_store :cookie_store, key: '_first_app_mysql_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FirstAppMysql::Application.config.session_store :active_record_store
